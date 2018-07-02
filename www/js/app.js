d9ec422694b5285bb9379f14441880723a3909c4
Noah Baron <noahbaron@gmail.com>
2018-07-02 16:29:51 -0700

    Updates

--- www/js/app.js
+++ www/js/app.js
@@ -10,7 +10,6 @@ var app  = new Framework7({
   statusbar: {
       iosOverlaysWebView: true,
       enabled: true,
-
   },
   // App root data
   data: function () {
@@ -51,9 +50,6 @@ $$(document).on('deviceready', function() {
           // Start tracking beacons
           initScan();
   
-          // Display refresh timer.
-          updateTimer = setInterval(updateBeacons, 500);
-  
           console.log(device);
   
           //permissions = cordova.plugins.permissions;
@@ -77,9 +73,6 @@ $$(document).on('deviceready', function() {
 
   // Dictionary of beacons.
   var beacons = {};
-
-  // Timer that displays list of beacons.
-  var updateTimer = null;
 
   function checkBluetoothPermissionCallback(status) {
       if (!status.hasPermission) {
@@ -149,11 +142,22 @@ $$(document).on('deviceready', function() {
 
   function toggleUnits(color) {
     var displaytempunits = localStorage.getItem('displayTempunits-' + color)||"°F";
-    if (displaytempunits == "°F") {
+    var displayfermunits = localStorage.getItem('displayFermunits-' + color)||"";
+    if (displaytempunits == "°F" && displayfermunits == "") {
         localStorage.setItem('displayTempunits-' + color,"°C");
+        localStorage.setItem('displayFermunits-' + color,"");
     }
-    if (displaytempunits == "°C") {
+    if (displaytempunits == "°C" && displayfermunits == "") {
+        localStorage.setItem('displayTempunits-' + color,"°C");
+        localStorage.setItem('displayFermunits-' + color,"°P");
+    }
+    if (displaytempunits == "°C" && displayfermunits == "°P") {
         localStorage.setItem('displayTempunits-' + color,"°F");
+        localStorage.setItem('displayFermunits-' + color,"°P");
+    }
+    if (displaytempunits == "°F" && displayfermunits == "°P") {
+        localStorage.setItem('displayTempunits-' + color,"°F");
+        localStorage.setItem('displayFermunits-' + color,"");
     }
   }
     
@@ -219,25 +223,30 @@ $$(document).on('deviceready', function() {
                          addtoScan(beacon);
                          break;
                   }
+                  //setup HD tilt
                 if (beacon.minor > 2000){
                     beacon.uncalTemp = beacon.major / 10;
-                    beacon.uncalSG = (beacon.minor / 10000).toFixed(4);;
+                    localStorage.setItem('uncalTemp-' + beacon.Color, beacon.uncalTemp);
+                    beacon.uncalSG = (beacon.minor / 10000).toFixed(4);
+                    localStorage.setItem('uncalSG-' + beacon.Color, beacon.uncalSG);
+                    localStorage.setItem('uncalTemp-' + beacon.Color, beacon.uncalTemp);
                     beacon.hd = true;
                 } else {
+                //setup SD tilt
                   beacon.uncalTemp = beacon.major;
+                  localStorage.setItem('uncalTemp-' + beacon.Color, beacon.uncalTemp);
                   beacon.uncalSG = (beacon.minor / 1000).toFixed(3);
+                  localStorage.setItem('uncalSG-' + beacon.Color, beacon.uncalSG);
+                  localStorage.setItem('uncalTemp-' + beacon.Color, beacon.uncalTemp);
                   beacon.hd = false;
                 }
-                  //add display units
-                  beacon.displayTempunits = localStorage.getItem('displayTempunits-' + beacon.Color)||"°F";
-                  beacon.displayTemp = localStorage.getItem('displayTemp-' + beacon.Color)||beacon.uncalTemp;
-                  beacon.uncalPlato = 1111.14 * beacon.uncalSG - 630.272 * beacon.uncalSG * beacon.uncalSG + 135.997 * beacon.uncalSG * beacon.uncalSG * beacon.uncalSG - 616.868;
                   //set key by UUID
                   var key = beacon.uuid;
                   beacons[key] = beacon;
                   //console.log(beacons);
               }
           }
+      updateBeacons();
       };
 
       // Set the delegate object to use.
@@ -257,9 +266,27 @@ $$(document).on('deviceready', function() {
     for (var key in beacons) {
     var beacon = beacons[key];
     var currentTime = Date.now();
+    
+    //add display value and units
+    beacon.displayTempunits = localStorage.getItem('displayTempunits-' + beacon.Color)||"°F";
+    switch (beacon.displayTempunits){
+        case "°F" : beacon.uncaldisplayTemp = beacon.uncalTemp;
+        break;
+        case "°C"  : beacon.uncaldisplayTemp = ((beacon.uncalTemp - 32) * 5 / 9).toFixed(1);
+        break;
+    }
+    beacon.displayFermunits = localStorage.getItem('displayFermunits-' + beacon.Color)||"";
+    switch (beacon.displayFermunits) {
+        case "" : beacon.uncaldisplayFerm = beacon.uncalSG;
+        break;
+        case "°P" : beacon.uncaldisplayFerm = (1111.14 * beacon.uncalSG - 630.272 * beacon.uncalSG * beacon.uncalSG + 135.997 * beacon.uncalSG * beacon.uncalSG * beacon.uncalSG - 616.868).toFixed(1);
+        break;
+    }
+    
     //setup tilt cards (generate new card once for each Tilt found)
     var foundBeacons = localStorage.getItem('foundbeacons');
     var foundBeaconsArray = foundBeacons.split(",");
+    //reset list of tilt cards if new tilt color found
     if (foundBeaconsArray.indexOf(beacon.Color) < 0){
         foundBeaconsArray.push(beacon.Color);
         localStorage.setItem('foundbeacons',foundBeaconsArray);
@@ -267,13 +294,78 @@ $$(document).on('deviceready', function() {
         var tiltCard  = $$('#tiltCard').html(displayhtml);
         var foundBeaconsArraylength = foundBeaconsArray.length;
         //setup javascript for each card
-        for (var i = 0; i < foundBeaconsArraylength; i++) {
+        for (var i = 1; i < foundBeaconsArraylength; i++) {
         //set up buttons
+        console.log(beacon.Color);
         $$('#unitstoggle-' + foundBeaconsArray[i]).on('click', function (e) {
-            var color = e.target.id.split("-");
-            console.log('clicked ' + color[1]);
-            toggleUnits(color[1]);
+            var unitscolor = e.currentTarget.id.split("-");
+            //console.log('clicked ' + unitscolor[1]);
+            toggleUnits(unitscolor[1]);
+            updateBeacons();
           });
+        $$('#calprompt-' + foundBeaconsArray[i]).on('click', function (e) {
+            var calcolor = e.currentTarget.id.split("-");
+            //console.log('clicked ' + calcolor[1]);
+            app.dialog.prompt('Enter actual SG/Concentration or tap "Cancel" to calibrate temperature:', 'Calibrate TILT | ' + calcolor[1], function (actual) {
+             var actualSGpoints = localStorage.getItem('actualSGpoints-' + calcolor[1])||'-0.001,1.000,10.000';
+             var actualSGpointsArray = actualSGpoints.split(',');
+             var uncalSGpoints = localStorage.getItem('uncalSGpoints-' + calcolor[1])||'-0.001,1.000,10.000';
+             var uncalSGpointsArray = uncalSGpoints.split(',');
+             var actualSGpoint = String(Number(actual).toFixed(3));
+             var uncalSGpoint = localStorage.getItem('uncalSG-' + calcolor[1]);
+             //add uncal. point only if actual doesn't already exist, otherwise replace with new uncal. point
+             var calSGindex = actualSGpointsArray.indexOf(actualSGpoint);
+             if (Number(actual) > 0.500 && Number(actual) < 2.000){
+              if (calSGindex < 0){
+                 actualSGpointsArray.push(actualSGpoint);
+                 actualSGpointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('actualSGpoints-' + calcolor[1], actualSGpointsArray);
+                 uncalSGpointsArray.push(uncalSGpoint);
+                 uncalSGpointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('uncalSGpoints-' + calcolor[1], uncalSGpointsArray);
+                 app.toast.create({text: 'Set ' + uncalSGpoint + ' (uncal.) to ' + actualSGpoint + ' (actual)', icon: '<i class="material-icons">adjust</i>', position: 'center', closeTimeout: 4000}).open();
+              } else{
+                 localStorage.setItem('actualSGpoints-' + calcolor[1], actualSGpointsArray);
+                 uncalSGpointsArray.splice(calSGindex, 1, uncalSGpoint);
+                 uncalSGpointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('uncalSGpoints-' + calcolor[1], uncalSGpointsArray);
+                 app.toast.create({text: 'Reset ' + uncalSGpoint + ' (uncal.) to ' + actualSGpoint + ' (actual)', icon: '<i class="material-icons">adjust</i>', position: 'center', closeTimeout: 4000}).open();
+             }
+            }else{
+                app.dialog.alert('The calibration point ' + actual + 'is out of range or not a number. Please try again.', 'Calibration Error');
+            }
+              }, function () {
+                app.dialog.prompt('Enter actual temperature:', 'Calibrate TILT | ' + calcolor[1], function (actualTemp){
+               var actualTemppoints = localStorage.getItem('actualTemppoints-' + calcolor[1])||'-0.001,1.000,10.000';
+               var actualTemppointsArray = actualTemppoints.split(',');
+               var uncalTemppoints = localStorage.getItem('uncalTemppoints-' + calcolor[1])||'0,1000';
+               var uncalTemppointsArray = uncalTemppoints.split(',');
+               var actualTemppoint = String(Number(actualTemp).toFixed(0));
+               var uncalTemppoint = localStorage.getItem('uncalTemp-' + calcolor[1]);
+               //add uncal. point only if actual doesn't already exist, otherwise replace with new uncal. point
+               var calTempindex = actualTemppointsArray.indexOf(actualTemppoint);
+             if (Number(actualTemp) > -1 && Number(actualTemp) < 213){
+              if (calTempindex < 0){
+                 actualTemppointsArray.push(actualTemppoint);
+                 actualTemppointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('actualTemppoints-' + calcolor[1], actualTemppointsArray);
+                 uncalTemppointsArray.push(uncalTemppoint);
+                 uncalTemppointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('uncalTemppoints-' + calcolor[1], uncalTemppointsArray);
+                 app.toast.create({text: 'Set ' + uncalTemppoint + ' (uncal.) to ' + actualTemppoint + ' (actual)', icon: '<i class="material-icons">adjust</i>', position: 'center', closeTimeout: 4000}).open();
+              } else{
+                 localStorage.setItem('actualTemppoints-' + calcolor[1], actualTemppointsArray);
+                 uncalTemppointsArray.splice(calTempindex, 1, uncalTemppoint);
+                 uncalSGpointsArray.sort(function(a, b){return a-b;});
+                 localStorage.setItem('uncalTemppoints-' + calcolor[1], uncalTemppointsArray);
+                 app.toast.create({text: 'Reset ' + uncalTemppoint + ' (uncal.) to ' + actualTemppoint + ' (actual)', icon: '<i class="material-icons">adjust</i>', position: 'center', closeTimeout: 4000}).open();
+             }
+            }else{
+                app.dialog.alert('The calibration point ' + actualTemp + 'is out of range or not a number. Please try again.', 'Calibration Error');
+            }
+                });
+                });
+            });       
         }
     }
     
@@ -287,11 +379,14 @@ $$(document).on('deviceready', function() {
     //initialize display units
     //update data fields in Tilt card template
     $$('#uncalSG' + beacon.Color).html(beacon.uncalSG);
+    $$('#uncaldisplayFerm+displayFermunits' + beacon.Color).html(String(beacon.uncaldisplayFerm) + beacon.displayFermunits);
     $$('#uncalTemp' + beacon.Color).html(beacon.uncalTemp);
-    $$('#displayTemp+displayTempunits' + beacon.Color).html(String(beacon.displayTemp) + beacon.displayTempunits);
+    $$('#uncaldisplayTemp+displayTempunits' + beacon.Color).html(String(beacon.uncaldisplayTemp) + beacon.displayTempunits);
     $$('#numberSecondsAgo' + beacon.Color).html(beacon.numberSecondsAgo);
     $$('#displayRSSI' + beacon.Color).html(beacon.displayRSSI);
     $$('#displaytimeStamp' + beacon.Color).html(beacon.displaytimeStamp);
+    $$('#percentScaleSG' + beacon.Color).css('width', String((beacon.uncalSG - 0.980) / (1.150 - 0.980) * 100) + "%");
+    $$('#percentScaleTemp' + beacon.Color).css('width', String((beacon.uncalTemp - 0) / (185 - 0) * 100) + "%");
     };
 }
 
