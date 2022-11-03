2e9648556936f5a0057df05d310836ad66ad0b56
Noah Baron <noahbaron@gmail.com>
2022-11-03 14:56:41 -0700

    tweaks

--- platforms/android/app/build/generated/source/buildConfig/debug/com/baronbrew/tilthydrometerf7/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/debug/com/baronbrew/tilthydrometerf7/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
   public static final String APPLICATION_ID = "com.baronbrew.tilthydrometerf7";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 10080;
-  public static final String VERSION_NAME = "1.0.80";
+  public static final int VERSION_CODE = 10085;
+  public static final String VERSION_NAME = "1.0.85";
 }
